491fc5a47f19056863ebb7fb8e274e165ccc3788
Pong <samfunghp@gmail.com>
2019-12-13 09:38:14 +0800

    fix scripts

--- scripts/mongo-export.sh
+++ scripts/mongo-export.sh
@@ -1,1 +1,6 @@
-docker exec -i mongo mongoexport -h localhost:27017 -d fullstack -c users > users.json
+
+#!/bin/bash
+
+# sh scripts/mongo-export.sh <collection name> <output file>
+
+docker exec -i mongo mongoexport -h localhost:27017 -d fullstack -c $1 > $2
