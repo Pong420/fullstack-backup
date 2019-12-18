68d69a888ee15fa6d4f39249dc434d5e174c46b1
Pong <samfunghp@gmail.com>
2019-12-18 17:43:54 +0800

    chore: make it better

--- typings/src/index.ts
+++ typings/src/index.ts
@@ -1,1 +1,8 @@
-export * from './typings';
+export * from './user';
+export * from './product';
+export * from './order';
+
+export interface Schema$Timestamp {
+  createdAt: string;
+  updatedAt: string;
+}
