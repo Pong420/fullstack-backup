24dabd964ea2a00c4281407c1a5ccb13736a36b3
Pong <samfunghp@gmail.com>
2019-12-25 21:30:44 +0800

    chore(common): simplify useActions hooks

--- packages/common/src/hooks/useActions.ts
+++ packages/common/src/hooks/useActions.ts
@@ -1,42 +1,26 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { useMemo } from 'react';
-import { Action } from 'redux';
+import { AnyAction } from 'redux';
 import { useDispatch } from 'react-redux';
 
 interface Actions {
-  [k: string]: (params?: any) => Action;
+  [k: string]: (...args: any[]) => AnyAction;
 }
 
-// https://stackoverflow.com/questions/50374908/transform-union-type-to-intersection-type
-type UnionToIntersection<U> = (U extends any
-  ? (k: U) => void
-  : never) extends ((k: infer I) => void)
-  ? I
-  : never;
-
-type GetFunction<Fn extends (params?: any) => any> = Parameters<
-  Fn
->[0] extends undefined
-  ? (() => void)
-  : UnionToIntersection<Parameters<Fn>[0]> extends undefined
-  ? ((params?: Parameters<Fn>[0]) => void)
-  : ((params: Parameters<Fn>[0]) => void);
-
-export function useActions<A extends Actions>(actions: A) {
+export function useActions<A extends Actions>(actions: A): A {
   const dispatch = useDispatch();
 
   return useMemo(() => {
-    const handler = {} as any;
+    const handler: any = {};
 
     for (const key in actions) {
-      handler[key] = (params?: any) => {
-        dispatch(actions[key](params));
+      const action = actions[key];
+      handler[key] = (...args: Parameters<typeof action>) => {
+        dispatch(action(...args));
       };
     }
 
-    return handler as {
-      [Key in keyof A]: GetFunction<A[Key]>;
-    };
+    return handler as A;
   }, [actions, dispatch]);
 }
