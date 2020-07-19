ca9e33669c831217c39de8dd1b83bf3c36ab7d8f
Pong <samfunghp@gmail.com>
2020-07-19 21:19:07 +0800

    chore(app): make it better

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
-import { App } from './src';
+import { useFonts } from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
+import { Login } from './src/pages/Login';
+import { fonts } from './src/components/Text';
 
 export default function () {
-  return <App />;
+  let [fontsLoaded] = useFonts(fonts);
+
+  if (fontsLoaded) {
+    return (
+      <>
+        <StatusBar style="auto" />
+        <Login />
+      </>
+    );
+  }
+
+  return null;
 }
